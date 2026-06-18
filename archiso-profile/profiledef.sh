e09392deab5f471a8f1c11fc310be7310dca7683
Jachyyeu <jachymjalovecky@icloud.com>
2026-06-18 18:23:19 +0200

    fix(archiso): remove missing files from profiledef permissions
    - profiledef.sh referenced archiso default files that don't exist
      in this profile (/root/.automated_script.sh, /root/.gnupg, etc.)
    - This likely caused mkarchiso to fail during ISO build

--- archiso-profile/profiledef.sh
+++ archiso-profile/profiledef.sh
@@ -17,11 +17,6 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/share/altos/installer/install.sh"]="0:0:755"
   ["/usr/share/altos/installer/live-bridge.sh"]="0:0:755"
   ["/usr/share/altos/first-boot/wizard.sh"]="0:0:755"
